0030827b6ad6b03ba451c7e8dbd29418b1d951fe
Jed Aureus Gonzales <4092668+jedau@users.noreply.github.com>
2026-02-05 17:56:21 +0800

    refactor: updated checkboxes.steps.ts to the new template

--- test/steps/checkboxes.steps.ts
+++ test/steps/checkboxes.steps.ts
@@ -1,57 +1,30 @@
-import { Given, When, Then } from '@cucumber/cucumber'
 import { expect } from '@playwright/test'
-import { ICustomWorld } from '../support/world.js'
 import { CheckboxesPage } from '../../pages/CheckboxesPage.js'
+import { Given, Then, When } from '../support/step-helpers.js'
 
-Given('I am on the checkboxes page', async function (this: ICustomWorld) {
-  if (!this.page) {
-    throw new Error('Page is not initialized')
-  }
-
-  const checkboxesPage = new CheckboxesPage(this.page)
+Given('I am on the checkboxes page', async function () {
+  const checkboxesPage = this.getPageObject(CheckboxesPage)
   await checkboxesPage.goto()
 })
 
-When('I check checkbox {int}', async function (this: ICustomWorld, checkboxNumber: number) {
-  if (!this.page) {
-    throw new Error('Page is not initialized')
-  }
-
-  const checkboxesPage = new CheckboxesPage(this.page)
+When('I check checkbox {int}', async function (checkboxNumber: number) {
+  const checkboxesPage = this.getPageObject(CheckboxesPage)
   await checkboxesPage.check(checkboxNumber)
 })
 
-When('I uncheck checkbox {int}', async function (this: ICustomWorld, checkboxNumber: number) {
-  if (!this.page) {
-    throw new Error('Page is not initialized')
-  }
-
-  const checkboxesPage = new CheckboxesPage(this.page)
+When('I uncheck checkbox {int}', async function (checkboxNumber: number) {
+  const checkboxesPage = this.getPageObject(CheckboxesPage)
   await checkboxesPage.uncheck(checkboxNumber)
 })
 
-Then(
-  'checkbox {int} should be checked',
-  async function (this: ICustomWorld, checkboxNumber: number) {
-    if (!this.page) {
-      throw new Error('Page is not initialized')
-    }
+Then('checkbox {int} should be checked', async function (checkboxNumber: number) {
+  const checkboxesPage = this.getPageObject(CheckboxesPage)
+  const isChecked = await checkboxesPage.isChecked(checkboxNumber)
+  expect(isChecked).toBe(true)
+})
 
-    const checkboxesPage = new CheckboxesPage(this.page)
-    const isChecked = await checkboxesPage.isChecked(checkboxNumber)
-    expect(isChecked).toBe(true)
-  },
-)
-
-Then(
-  'checkbox {int} should be unchecked',
-  async function (this: ICustomWorld, checkboxNumber: number) {
-    if (!this.page) {
-      throw new Error('Page is not initialized')
-    }
-
-    const checkboxesPage = new CheckboxesPage(this.page)
-    const isChecked = await checkboxesPage.isChecked(checkboxNumber)
-    expect(isChecked).toBe(false)
-  },
-)
+Then('checkbox {int} should be unchecked', async function (checkboxNumber: number) {
+  const checkboxesPage = this.getPageObject(CheckboxesPage)
+  const isChecked = await checkboxesPage.isChecked(checkboxNumber)
+  expect(isChecked).toBe(false)
+})
